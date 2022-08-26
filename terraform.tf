be60fdd70cd216c405652d5562a14453ad878397
Kerim Satirli <k.satirli@gmail.com>
2022-08-26 18:59:29 +0200

    adds Terraform Cloud backend

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,12 @@
 terraform {
+  cloud {
+    organization = "workloads"
+
+    workspaces {
+      name = "website"
+    }
+  }
+
   # see https://www.terraform.io/docs/language/settings/index.html#specifying-provider-requirements
   required_providers {
     # see https://registry.terraform.io/providers/integrations/github/4.29.0/
