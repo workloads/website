e4b8535eaf13bb85d755feceef2c708162d2055b
Kerim Satirli <k.satirli@gmail.com>
2023-01-23 10:33:04 +0100

    bumps GitHub Provider from `5.15.0` to `5.16.0` and AWS Provider from `4.50.0` to `4.51.0

--- providers.tf
+++ providers.tf
@@ -1,3 +1,33 @@
+# The AWS Provider is set to retrieve configuration from the executing environment
+# see https://registry.terraform.io/providers/hashicorp/aws/latest/docs#schema
+provider "aws" {
+  region = "us-west-2"
+
+  # see https://registry.terraform.io/providers/hashicorp/aws/latest/docs#default_tags
+  default_tags {
+    tags = {
+      "github:url" = "https://github.com/workloads/website"
+    }
+  }
+}
+
+# The AWS Provider is set to retrieve configuration from the executing environment
+# see https://registry.terraform.io/providers/hashicorp/aws/latest/docs#schema
+# This aliased version of the provider is specifically limited to the `us-east-1`
+# region to allow for lifecycle operations for ACM resources.
+provider "aws" {
+  alias  = "certificate"
+  region = "us-east-1"
+
+  # see https://registry.terraform.io/providers/hashicorp/aws/latest/docs#default_tags
+  default_tags {
+    tags = {
+      "github:url" = "https://github.com/workloads/website"
+    }
+  }
+}
+
+# The GitHub Provider is set to retrieve configuration from the executing environment
 # see https://registry.terraform.io/providers/integrations/github/latest/docs
 provider "github" {
   owner = var.github_owner
