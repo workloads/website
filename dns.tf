bad93296a375d982426c0ecf336c9e30ca8fa77f
Kerim Satirli <k.satirli@gmail.com>
2023-05-28 15:38:49 +0200

    cleanup

--- dns.tf
+++
@@ -1,8 +0,0 @@
-# The Route53 Zone Data Source is solely used to retrieve a Route53 Zone ID.
-# see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/route53_zone
-data "aws_route53_zone" "domains" {
-  # see https://developer.hashicorp.com/terraform/language/meta-arguments/for_each
-  for_each = var.domains
-
-  name = each.value
-}
