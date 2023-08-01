8c17d6b7ebb29f1d06476b790a7861f9bfa22475
Kerim Satirli <k.satirli@gmail.com>
2023-08-01 11:31:12 +0200

    bumps providers

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,12 @@
 terraform {
   # see https://developer.hashicorp.com/terraform/language/settings/terraform-cloud
   cloud {
-    # see https://developer.hashicorp.com/terraform/cli/cloud/settings#organization
+    # see https://app.terraform.io/app/workloads/workspaces
+    # and https://developer.hashicorp.com/terraform/cli/cloud/settings#organization
     organization = "workloads"
 
-    # see https://developer.hashicorp.com/terraform/cli/cloud/settings#workspaces
+    # see https://app.terraform.io/app/workloads/website
+    # and https://developer.hashicorp.com/terraform/cli/cloud/settings#workspaces
     workspaces {
       name = "website"
     }
@@ -12,16 +14,16 @@ terraform {
 
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/hashicorp/aws/5.9.0
+    # see https://registry.terraform.io/providers/hashicorp/aws/5.10.0
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.9.0, < 6.0.0"
+      version = ">= 5.10.0, < 6.0.0"
     }
 
-    # see https://registry.terraform.io/providers/integrations/github/5.31.0
+    # see https://registry.terraform.io/providers/integrations/github/5.32.0
     github = {
       source  = "integrations/github"
-      version = ">= 5.31.0, < 6.0.0"
+      version = ">= 5.32.0, < 6.0.0"
     }
 
     # see https://registry.terraform.io/providers/hashicorp/local/2.4.0
